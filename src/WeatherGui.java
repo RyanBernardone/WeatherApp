37e643731fba7ef0e629d1ed7c7feca7f4250a59
Ryan <ryan.bernardone@gmail.com>
2026-02-24 20:24:29 -0300

    feat: created the search bar and the search button.

--- src/WeatherGui.java
+++ src/WeatherGui.java
@@ -9,7 +9,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class WeatherGui extends JFrame{
-    public WeatherGui() {
+    public WeatherGui(){
         super("Weather App");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(WeatherConstants.APPGUI_WIDTH, WeatherConstants.APPGUI_HEIGHT);
@@ -17,5 +17,28 @@ public class WeatherGui extends JFrame{
         setLocationRelativeTo(null);
         setLayout(null);
 
+        addGuiComponents();
+    }
+
+    private void addGuiComponents(){
+        JTextField searchField = new JTextField();
+        searchField.setBounds(15, 15, 351, 45);
+        add(searchField);
+
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
+    }
+
+    private ImageIcon loadImage(String resourcePath){
+        try{
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+            return new ImageIcon(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Could not find resource");
+        return null;
     }
 }
